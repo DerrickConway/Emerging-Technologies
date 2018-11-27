e75ad7e85655025e5d5698dd4d62abd85d699dd2
Derrick Conway <derrick.conway15@gmail.com>
2018-11-27 14:26:54 +0000

    indentation fixed

--- 4.Digit-Recognition-Notebook/digitrec.py
+++ 4.Digit-Recognition-Notebook/digitrec.py
@@ -1,35 +1,31 @@
-#import the following python libraries:
 import gzip
-#import PIL.Image as pil
-from PIL import Image  # image library
+from PIL import Image
 import numpy as np
 
-
-# Parse all images in the given file to a list.
 def parse_images(images_path):
-		# Open the image gzip in read bytes mode.
-
     with gzip.open(images_path, 'rb') as file:
-		# Read the first four bytes i.e. the magic number.
-	    magic = file.read(4)
-        #magic = int.from_bytes(magic, 'big')
-        print("Magic number: " +, magic)
 
-		# Read the next four bytes i.e. the number of images.
-	    number_of_images = int.from_bytes(file.read(4), 'big')
-        print("Number of images: ", number_of_images)
+        magic = int.from_bytes(file.read(4), 'big')
+        print(" Magic number: " + str(magic))
 
-		# Read the next four bytes i.e. the number of rows.
-	    rows = int.from_bytes(file.read(4), 'big')
-        print("Number of rows: ", rows)
 
-		# Read the next four bytes i.e. the number of cols.
-	    columns = int.from_bytes(file.read(4), 'big')
-        print("Number of cols: ", columns)
-    
+        number_of_Images = int.from_bytes(file.read(4), 'big')
+        print("Number of Images: " + str(number_of_Images))
 
-print() # break
+
+        rows = int.from_bytes(file.read(4), 'big')
+        print("Number of rows; " + str(rows))
+
+
+        columns = int.from_bytes(file.read(4), 'big')
+        print("Number of Columns: " + str(columns))
+
+
+
+      
+
+#print()
+
         
-			
-			
-	
+
+
