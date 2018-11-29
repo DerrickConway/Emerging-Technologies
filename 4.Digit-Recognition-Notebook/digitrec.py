1ed2cc26324562cf23605fd67dd29fdb266775f6
Derrick Conway <derrick.conway15@gmail.com>
2018-11-29 10:22:03 +0000

    still working on it

--- 4.Digit-Recognition-Notebook/digitrec.py
+++ 4.Digit-Recognition-Notebook/digitrec.py
@@ -2,7 +2,13 @@ import gzip
 from PIL import Image
 import numpy as np
 
+#self.name = name
+
+
+
+
 def parse_images(images_path):
+    
     with gzip.open(images_path, 'rb') as file:
 
         magic = int.from_bytes(file.read(4), 'big')
@@ -19,6 +25,25 @@ def parse_images(images_path):
 
         columns = int.from_bytes(file.read(4), 'big')
         print("Number of Columns: " + str(columns))
+
+      #  buffer = file.read(rows * columns * number_of_Images)
+     #   data = np.frombuffer(buffer, dtype=np.uint8)
+    #    self.images = data.reshape(number_of_Images, rows, columns)
+
+
+def parse_labels(labels_path):
+    with gzip.open(labels_path, 'rb') as file:
+
+        magic = int.from_bytes(file.read(4), 'big')
+        print("Magic number: " + str(magic))
+
+
+        number_of_labels = int.from_bytes(file.read(4), 'big')
+        print("Number of labels: " + str(number_of_labels))
+
+
+
+
 
 
 
