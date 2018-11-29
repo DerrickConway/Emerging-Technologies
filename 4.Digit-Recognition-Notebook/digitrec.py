8b7c3a7a716292e3c1de3ee7f45aaa4e6b49167f
Derrick Conway <derrick.conway15@gmail.com>
2018-11-29 14:08:42 +0000

    for loop problem

--- 4.Digit-Recognition-Notebook/digitrec.py
+++ 4.Digit-Recognition-Notebook/digitrec.py
@@ -6,13 +6,14 @@ import numpy as np
 
 
 #class MnistDataSet:
-def __init__(self, name, image_Path, lable_path):
-    self.name = name
+#def __init__(self, name, image_Path, lable_path):
+   # self.name = name
 
-    self.parse_images(image_Path)
-    self.parse_labels(lable_path)
+    #self.parse_images(image_Path)
+ #   self.parse_labels(lable_path)
 
-def parse_images(self,images_path):
+#def parse_images(self, images_path):
+def parse_images(images_path):
             
     with gzip.open(images_path, 'rb') as file:
         magic = int.from_bytes(file.read(4), 'big')
@@ -31,31 +32,53 @@ def parse_images(self,images_path):
         columns = int.from_bytes(file.read(4), 'big')
         print("Number of Columns: " + str(columns))
 
-        buffer = file.read(rows * columns * number_of_Images)
-        data = np.frombuffer(buffer, dtype=np.uint8)
-        self.images = data.reshape(number_of_Images, rows, columns)
+       # buffer = file.read(rows * columns * number_of_Images)
+       # data = np.frombuffer(buffer, dtype=np.uint8)
+      #  self.images = data.reshape(number_of_Images, rows, columns)
+
+        images = []
+        for i in range(number_of_Images):
+            rows = []
+            for r in range(rows):
+                columns = []
+                for c in range(columns):
+                    columns.append(int.from_bytes(file.read(1), 'big'))
+                rows.append(columns)
+            images.append(rows)
+        return images
+
+print()
+
+trainImages = parse_images('train-images-idx3-ubyte.gz')
+testImages = parse_images('t10k-images-idx3-ubyte.gz')
 
 
-def parse_labels(labels_path):
+
+
+def parse_labels( labels_path):
     with gzip.open(labels_path, 'rb') as file:
 
         magic = int.from_bytes(file.read(4), 'big')
         print("Magic number: " + str(magic))
 
-
         number_of_labels = int.from_bytes(file.read(4), 'big')
         print("Number of labels: " + str(number_of_labels))
 
+        labels =[file.read(1) for i in range(number_of_labels)]
+        labels = [int.from_bytes(label, 'big') for label in labels]
+
+        return labels
+print()
 
 
+translabels = parse_labels('train-labels-idx1-ubyte.gz')
+testlabels = parse_labels('t10k-labels-idx1-ubyte.gz')
 
+    
 
+      #  buffer = file.read(number_of_labels)
+       # self.labels = np.frombuffer(buffer, astype (np.unit8))
 
+   # images = []
 
-      
-
-#print()
-
-        
-
-
+        #print()
